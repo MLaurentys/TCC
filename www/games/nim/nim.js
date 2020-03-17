682a6f41a50539d0a7773f4b23d15919d948f88d
MLaurentys <matheus_t.laurentys@hotmail.com>
2020-03-17 16:31:56 -0300

    few changes

--- www/games/nim/nim.js
+++ www/games/nim/nim.js
@@ -3,9 +3,24 @@
 //Defines
 var play_button = "<button id=\"generate\" onclick=\"make_play()\">Make Move</button>"
 function make_pile(i){return `Size of pile ${i + 1}: <input id=\"pile_${i+1}\" value="0"></input><br>`}
-function make_pile_description(ind, val, i){return `<span id=\"pile_ds_${i+1}\">${make_pile_text()}</span><br>`}
-function make_pile_text(ind, val, i){return `| ${ind} | ${val} | Remove: <input id=\"rem_${i+1}\" value=\"0\"></input>`}
+function make_pile_description(i){return `<span id=\"pile_ds_${i+1}\">${make_pile_text(i)}</span><br>`}
 
+//Helpers
+function make_pile_text(i){
+    let ind = pad(5, i+1, 1)
+    let val = pad(9, piles_vals[i], 1)
+    return `| ${ind} | ${val} | Remove: <input id=\"rem_${i+1}\" value=\"0\"></input>`
+}
+function pad(pad_total, str, pad_right){
+    let sb = ""
+    for (let i = 0; i < pad_total; i++) {sb+="_"}
+    sb+=String(str)
+    for (let i = 0; i < pad_right; i++) {sb+="_"}
+    return String((sb).slice(-pad_total))
+}
+function read_one_int(str, si){
+    
+}
 //Accessors
 var num_piles_box = document.getElementById("num_piles")
 var piles_defs = document.getElementById("piles")
@@ -17,6 +32,9 @@ var total_piles = 0
 var piles_vals = []
 var active_piles = 0
 
+function createFromString(){
+
+}
 function change_number(newValue){
     num_piles = parseInt(newValue)
     num_piles_box.value = num_piles
@@ -30,16 +48,14 @@ function make_piles_defs(){
     }
 }
 function make_board(){
-    total_piles = num_piles
+    total_piles = num_piles 
     active_piles = 0
     board.innerHTML = "Piles | Remaining <br><br>"
     for (let i = 0; i < num_piles; i++) {
         let val = document.getElementById(`pile_${i+1}`).value
         piles_vals.push(val)
         if (val > 0) active_piles++
-        let pile_ind = pad(5, i+1, 1)
-        let pile_val = pad(9, val, 1)
-        let element = make_pile_description(pile_ind, pile_val, i)
+        let element = make_pile_description(i)
         board.innerHTML += element
     }
     board.innerHTML += play_button
@@ -59,13 +75,6 @@ function make_play(){
 }
 function finish_play(pile_ind, pile_rem){
     piles_vals[pile_ind] -= pile_rem
-    document.getElementById(`pile_ds_${pile_ind+1}`).innerHTML = pile_text(pile_ind, pile_rem)
+    document.getElementById(`pile_ds_${pile_ind+1}`).innerHTML = make_pile_text(pile_ind)
 }
 
-function pad(pad_total, str, pad_right){
-    let sb = ""
-    for (let i = 0; i < pad_total; i++) {sb+="_"}
-    sb+=str
-    for (let i = 0; i < pad_right; i++) {sb+="_"}
-    return String((sb).slice(-pad_total))
-}
