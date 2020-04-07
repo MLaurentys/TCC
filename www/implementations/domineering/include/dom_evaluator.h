7e15903dc824e6e0cddc8ce3b39ad050de782c8f
MLaurentys <matheus_t.laurentys@hotmail.com>
2020-04-07 19:37:01 -0300

    implemented get_moves

--- www/implementations/domineering/include/dom_evaluator.h
+++ www/implementations/domineering/include/dom_evaluator.h
@@ -10,13 +10,15 @@
 
 using std::tuple;
 using std::vector;
+using std::get;
 
 class DomEvaluator{
     public:
     float evaluate(const Domineering&);
     
     private:
-    vector<semi_board> break_configuration(const Domineering&); 
+    tuple<vector<move>, vector<move>> get_moves (const int_mat&, const semi_board&);
+    tuple<int_mat, vector<semi_board>> break_configuration(const Domineering&);
     float evaluate_game_fixed(const Domineering&, semi_board);
     float eval_rec(const Domineering&);
 };
--- www/implementations/domineering/src/dom_evaluator.cpp
+++ www/implementations/domineering/src/dom_evaluator.cpp
@@ -5,7 +5,26 @@ float DomEvaluator::evaluate(const Domineering& game){
     return 0.0f;
 }
 
-vector<semi_board> DomEvaluator::break_configuration(const Domineering& g){
+tuple<vector<move>, vector<move>> DomEvaluator::get_moves(
+        const int_mat& mat,
+        const semi_board& sb) {
+    vector<move> left,right;
+    int r_it = 0;
+    //left moves verticaLLy
+    for (int i = get<0>(sb); i < get<2>(sb); ++i)
+        for (int j = std::max(1,get<1>(sb)); j < get<3>(sb); ++j)
+            if (mat[i][j] == get<4>(sb) && mat[i][j-1] == get<4>(sb))
+                left.push_back({{i,j-1}, {i,j}});
+    //right moves hoRizontally
+    for (int i = std::max(1, get<0>(sb)); i < get<2>(sb); ++i)
+        for (int j = get<1>(sb); j < get<3>(sb); ++j)
+            if (mat[i][j] == get<4>(sb) && mat[i-1][j] == get<4>(sb))
+                right.push_back({{i-1,j}, {i,j}});
+    return {left, right};
+}
+
+tuple<int_mat, vector<semi_board>> DomEvaluator::break_configuration(
+        const Domineering& g) {
     auto rems = g.get_sorted_removes();
     int n = g.wid(), m = g.hei(), g_amt;
     int_mat mat;
@@ -30,12 +49,5 @@ vector<semi_board> DomEvaluator::break_configuration(const Domineering& g){
                 get<3>(ret[g]) = std::max(get<3>(ret[g]), i - get<1>(ret[g]));
         }
     }
-    return ret;
+    return {mat, ret};
 }
-
-/*
-
-
-
-
-*/
